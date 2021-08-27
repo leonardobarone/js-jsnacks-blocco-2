aa9eb17d371f91c9a252aa48fb6330f87b5b7a32
leonardobarone <leonardobarone26@gmail.com>
2021-08-27 17:53:52 +0200

    aggiustamento snacks-4 con samu

--- snacks-4/script.js
+++ snacks-4/script.js
@@ -1,17 +1,16 @@
 // Crea un array di numeri interi
-var interi = [
-    1, 2, 2, 3, 4
-];
 // e fai la somma di tutti gli elementi che sono in posizione (indice dell'array) dispari
+var interi = [
+    1, 2, 3, 4, 5, 6, 7, 8
+];
 
 var somma = 0;
 
+
 for (var i = 0; i < interi.length; i++) {
     
-    if (interi[i] % 2 == 1) {
-        
-        somma = somma + interi[i];
-
+    if (i % 2 == 1) {
+        somma = somma + i;
     }
 
 }
