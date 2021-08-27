23fa940fc6f035b34d1285064353f95c52d2c139
leonardobarone <leonardobarone26@gmail.com>
2021-08-27 11:46:15 +0200

    fatto con while senza convalidare numero

--- snacks-1/script.js
+++ snacks-1/script.js
@@ -1,18 +1,33 @@
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 
-var array = [];
+// var array = [];
 
-for (var i = 1; i < 6; i++) {
-    var numero = parseInt(prompt("dammi un numero!"));
-    array.push(numero);
-}
+// for (var i = 1; i < 6; i++) {
+//     var numero = parseInt(prompt("dammi un numero!"));
+//     array.push(numero);
+// }
 
 
-arrayTot = array.reduce((a, b) => a + b, 0)
+// arrayTot = array.reduce((a, b) => a + b, 0)
 
-console.log(arrayTot);
+// console.log(arrayTot);
 
 
 // Il programma stampa la somma di tutti i numeri inseriti.
 
 // Esegui questo programma in due versioni, con il for e con il while.
+
+var i = 1;
+var array2 = [];
+
+while (i < 6) {
+
+    var numero = parseInt(prompt("dammi un numero!"));
+    array2.push(numero);
+
+    i++
+}
+
+arrayTot2 = array2.reduce((a, b) => a + b, 0)
+
+console.log(arrayTot2);
