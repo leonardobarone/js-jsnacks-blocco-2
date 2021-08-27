2f259963aabbc31daa48e836769b2da0777ab466
leonardobarone <leonardobarone26@gmail.com>
2021-08-27 16:53:20 +0200

    risolto con ale

--- snacks-5/script.js
+++ snacks-5/script.js
@@ -4,21 +4,26 @@
 
 
 // Crea due array che hanno un numero di elementi diversi.
-var primoArray = [1, 2, 3];
-var secondoArray = [1, 2, 3, 4, 5];
+var primoArray = [1, 2];
+var secondoArray = [1, 2, 3];
 
 while (primoArray.length < secondoArray.length) {
-
+    
     var random = Math.floor(Math.random() * 6);
-
     primoArray.push(random);
-
 }
 
-console.log(primoArray)
+while (primoArray.length > secondoArray.length) {
+    var random = Math.floor(Math.random() * 6);
+    secondoArray.push(random);
+}
+
+console.log(primoArray);
+console.log(secondoArray);
 
 
 // Aggiungi elementi casuali all’array che ha meno elementi,
 
 
 // fino a quando ne avrà tanti quanti l’altro.
+
