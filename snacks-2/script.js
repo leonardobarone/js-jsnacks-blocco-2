8282015247eb7c014bda206e9fd897ce5dd4c81f
leonardobarone <leonardobarone26@gmail.com>
2021-08-27 12:20:06 +0200

    manca validazione

---
+++ snacks-2/script.js
@@ -0,0 +1,19 @@
+// Inserisci un numero, se è pari stampa il numero,
+
+var numero = parseInt(prompt("Inserisci un numero pari"));
+
+while ( !isNaN(numero)) {
+
+numero = parseInt(prompt("Inserisci un numero pari"));
+
+}
+
+if (numero % 2 == 0) {
+    
+    console.log(numero);
+
+} else {
+    console.log(numero + 1);
+}
+
+// se è dispari stampa il numero successivo
