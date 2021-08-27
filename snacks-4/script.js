df8cf953f988fdfc58e18ff37d56fae8e059d819
leonardobarone <leonardobarone26@gmail.com>
2021-08-27 15:53:37 +0200

    snacsk 4

--- snacks-4/script.js
+++ snacks-4/script.js
@@ -1,6 +1,6 @@
 // Crea un array di numeri interi
 var interi = [
-    1, 2, 3, 4, 5
+    1, 2, 2, 3, 4
 ];
 // e fai la somma di tutti gli elementi che sono in posizione (indice dell'array) dispari
 
@@ -8,7 +8,7 @@ var somma = 0;
 
 for (var i = 0; i < interi.length; i++) {
     
-    if (i % 2 == 1) {
+    if (interi[i] % 2 == 1) {
         
         somma = somma + interi[i];
 
